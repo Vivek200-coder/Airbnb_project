c9dc85195e07a5ddfb761bb105b6247386454e20
Vivek Jaiswal <vj8562266@gmail.com>
2026-06-01 12:25:54 +0530

    fix cloudinary env

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,11 +1,16 @@
+if(process.env.NODE_ENV != "production") {
+  require('dotenv').config();
+}
 const cloudinary = require('cloudinary').v2;
 const CloudinaryStorage = require('multer-storage-cloudinary');
+
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET
 });
+console.log("Cloud name:", process.env.CLOUD_NAME);
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
