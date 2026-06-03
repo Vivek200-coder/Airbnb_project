964d6493104bd537a8420752b5aa7188bf023b64
Vivek Jaiswal <vj8562266@gmail.com>
2026-06-03 11:44:55 +0530

    fix: correct CloudinaryStorage import for v2

--- cloudConfig.js
+++ cloudConfig.js
@@ -3,8 +3,7 @@ if(process.env.NODE_ENV != "production") {
 }
 
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-
+const CloudinaryStorage = require('multer-storage-cloudinary');
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
